45333a4e4097514447b8e2667b6b717e0a7c92a8
Mac <mac@Miriani.local>
2023-06-02 07:55:36 +0400

    fix old message but

--- src/components/sidebar/NavBar.tsx
+++ src/components/sidebar/NavBar.tsx
@@ -5,12 +5,15 @@ import { signOut } from "firebase/auth";
 import { Img } from "./NavBar.styles";
 import { useAuthCtx } from "../../store/AuthCtx";
 import { Button } from "../shared/Button";
+import { useChatCtx } from "../../store/ChatCtx";
 
 export const NavBar = () => {
   const { currentUser }: any = useAuthCtx();
+  const { setChatUser }: any = useChatCtx();
 
   const handleSignOut: MouseEventHandler<HTMLButtonElement> = () => {
     signOut(auth);
+    setChatUser(null);
   };
 
   return (
